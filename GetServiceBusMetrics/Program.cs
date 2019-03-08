6d71cbef1b20a8ab5e968f2610e0e7e0c6361b61
Jack Feng <shfen@microsoft.com>
2019-03-07 20:43:13 -0800

    able to receive messages

--- GetServiceBusMetrics/Program.cs
+++ GetServiceBusMetrics/Program.cs
@@ -26,27 +26,46 @@ namespace GetServiceBusMetrics
             //    }
             //};
 
-            ServiceBusNamespaceDetails serviceBusNamespaceDetails = new ServiceBusNamespaceDetails()
-            {
-                Namespace = namespaceConfiguration.Namespace,
-                NamespaceAuthKey = ""
-            };
-            {
-                ServiceUri = new Uri("sb://msfb-seatblocks-int-asb-wcus.servicebus.windows.net/"),
-                Credentials = new KeyFileCredentialConfiguration.ApSecureStore
-                {
-                    EncryptedKeyFilePath = "msfb-seatblocks-int-asb-wcus.servicebus-primarykey.txt.encr"
-                };
+            string ServiceBusUriScheme = "sb";
+            string RootManageSharedAccessKeyName = "RootManageSharedAccessKey";
+            string RootManageSharedAccessKeyValue = "64Ohm/hiKkNaHOLHPd63yLQ0QxlrCFvD9HGO1LSnQ3A=";
+            //var RootManageSharedAccessKey = new ApSecureStoreAzureStorageCredentialConfiguration()
+            //{
+            //    EncryptedKeyFilePath = "bsx-commsearch-asb-intmd-scu--primarykey.txt.encr"
+            //};
 
-            TokenProvider tokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider(RootManageSharedAccessKey, namespaceConfiguration.NamespaceAuthKey);
 
-            Uri uri = ServiceBusEnvironment.CreateServiceUri(ServiceBusUriScheme, namespaceConfiguration.Namespace, String.Empty);
+            string Namespace = "msfb-seatblocks-int-asb-wcus";
+
+
+            //{
+            //    ServiceUri = new Uri("sb://msfb-seatblocks-int-asb-wcus.servicebus.windows.net/"),
+            //    Credentials = new KeyFileCredentialConfiguration.ApSecureStore
+            //    {
+            //        EncryptedKeyFilePath = "msfb-seatblocks-int-asb-wcus.servicebus-primarykey.txt.encr"
+            //    };
+            
+
+            TokenProvider tokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider(RootManageSharedAccessKeyName, RootManageSharedAccessKeyValue);
+
+            Uri uri = ServiceBusEnvironment.CreateServiceUri(ServiceBusUriScheme, Namespace, String.Empty);
 
             var namespaceManager = new NamespaceManager(uri, tokenProvider);
             IEnumerable<QueueDescription> queuesDescriptions = namespaceManager.GetQueuesAsync().Result;
 
-            Console.WriteLine(queuesDescriptions.FirstOrDefault().MessageCountDetails.DeadLetterMessageCount);
+            foreach (var queuesDescription in queuesDescriptions)
+            {
+                Console.WriteLine("count of deadletter message is :" + queuesDescription.MessageCountDetails.DeadLetterMessageCount);
+                Console.WriteLine("count of acive message is :" + queuesDescription.MessageCountDetails.ActiveMessageCount);
 
+                Console.WriteLine("path is :" + queuesDescription.Path);
+                Console.WriteLine();
+            }
+
+
+            while (true)
+            {
+                
             }
         }
     }
